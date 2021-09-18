4f5945126a771ce3225ffb416d23a2d445d06c92
unknown <alexm1397@gmail.com>
2021-09-18 11:06:21 -0500

    quiz question elements in place, need to add timer, finish selectedAnswer func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,110 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
+const startButtonEl = document.getElementById('start-btn')
+const nextButtonEl = document.getElementById('next-btn')
+const questionContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('question')
+const answerButtonsEl = document.getElementById('answer-buttons');
+let shuffledQuestions, currentQuestionIndex = 0
+const questions = [
+    {
+     question: "What language gives a browser the basic markup to create a page on the internet?",
+     answers: [
+         {text: "CSS", correct: false},
+         {text: "Git", correct: false},
+         {text: "HTML", correct: true},
+         {text: "Markdown", correct: false}
+    ]},{
+        question: "What does 'CSS' stand for?",
+        answers: [
+            {text: "Casper's Style Sheet", correct: false},
+            {text: "Cascading Style Sheets", correct: true},
+            {text: "Cascading Stlye Source", correct: false},
+            {text: "Condescending Style Sheets", correct: false}
+        ]},{
+            question: "Are there a diffencs between the languages of Java and JavaScript?",
+            answers: [
+                {text: "Yes, Java is a PROGRAMMING LANGUAGE, while JavaScript is a SCRIPTING LANGUAGE", correct: true},
+                {text: "Yes, Java will only create applications to run in a Virtual Machine, while JavaScript will only run in a web browser",
+                            correct: false},
+                
+            ]}
+]
 
-startButton.addEventListener('click', startGame)
+
+startButtonEl.addEventListener('click', startGame)
+nextButtonEl.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('started')
-    startButton.classList.add('hide')
-
-
+    startButtonEl.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() -  .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove('hide')
+    setNextQuestion();
 }
 
 function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        //*Can i add answer.false to track wrong answer clicks? then use to target timer sub action
+    button.addEventListener('click', selectAnswer)
+    answerButtonsEl.appendChild(button)
+    })
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+        //WRITE IF LOOP FOR IF BUTTON IS CLICKED, SUB TIME IF WRONG, ADVANCE NEXT QUESTION IF ANY REMAIN
 }
 
 
 
-const questions = [
-    {
-     question: 'what is 2 + 2',
-     answers: [
-         {text: '4', correct: true},
-         {text: '22', correct: false},
-     ]}
+
+    //PREVIOUS SELECT ANSWERS(e) FUNC
+//     const selectedButton = e.target
+//     const correct = selectedButton.dataset.correct
+//     setStatusClass(document.body, correct)
+//     Array.from(answerButtonsEl.children).forEach(button => {
+//         setStatusClass(button, button.dataset.correct)
+//     })
+//     if (shuffledQuestions.length > currentQuestionIndex + 1) {
+//     nextButtonEl.classList.remove('hide')
+//     } else {
+//         startButtonEl.innerText = 'Restart'
+//         startButtonEl.classList.remove('hide')
+//     }
+// }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element) 
+    if (correct) {
+        element.classList.add('correct')
+    } else { 
+        element.classList.add('wrong')
+    }
+}
+        //for body color hue
+// function clearStatusClass(element) {
+//     element.classList.remove('correct')
+//     element.classList.remove('wrong')
+// }
+
+function resetState() {
+    nextButtonEl.classList.add('hide')
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
