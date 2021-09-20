1af61a14160f25786eda9257ccebe092d4bb609d
ai2 <alexm1397@gmail.com>
2021-09-19 22:53:10 -0500

    time subtracts, made progress on saving scores

---
+++ assets/js/scores.js
@@ -0,0 +1,9 @@
+const olEl = document.getElementsByClassName('score-list')
+
+
+
+function displayScores {
+    localStorage.getItem
+}
+
+displayScores();
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,9 @@ const questionEl = document.getElementById('question')
 const answerButtonsEl = document.getElementById('answer-buttons');
 var timerEl = document.getElementById('timer');
 let shuffledQuestions, currentQuestionIndex = 0;
-var timeLeft = 90;
-var time = 90;
+var time;
 var timerId;
-
+const highScoresArr = JSON.parse(localStorage.getItem("highScoresArr")) || []
 const questions = [
     {
      question: "What language gives a browser the basic markup to create a page on the internet?",
@@ -33,46 +32,25 @@ const questions = [
                 
             ]}
 ]
-
 startButtonEl.addEventListener('click', startGame)
-
 answerButtonsEl.addEventListener('click',() => {
-    // if (shuffledQuestions.length <= currentQuestionIndex + 1) {
-    //     clearInterval(countdown())
-    //     var timeLeft = 89;
-    //     return timeLeft;
-    // } else
-    resetState();
-    currentQuestionIndex++;
-    setNextQuestion();
-})
+    if (answerButtonsEl !== questions[currentQuestionIndex].answers.correct) { 
+        time -= 10;
+    }
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        currentQuestionIndex++;
+        resetState();
+        setNextQuestion();
+    } else { 
+        endGame();
+    }})
 
 
-// function countdown() {
-//     startTimeLeft = 90
-//     var timeInterval = setInterval(function(){
-//         if (timeLeft === 0) {
-//             timerEl.textContent = ' ';
-//             clearInterval(timeInterval);
-//             displayMessage();
-//             return;
-//             }
-//         if (timeLeft === 1) {
-//             timerEl.textContent = timeLeft + ' second remaining';
-//             } else {
-//             timerEl.textContent = timeLeft + ' seconds remaining';
-//             }
-//         timeLeft = startTimeLeft--
-//         return timeLeft;
-//         }, 1000);
-//     }
 function clockTick() {
-    if (shuffledQuestions.length >= currentQuestionIndex + 1) {
+    if (shuffledQuestions.length >= currentQuestionIndex + 1 && time >= 0) {
     time--;
-    timerEl.textContent = time;
-    if (time <= 0) {
-        stopGame()
-    }}
+    timerEl.textContent = time + " Seconds Remaining";
+    }
 }
 
 function startGame() { 
@@ -80,18 +58,11 @@ function startGame() {
     shuffledQuestions = questions.sort(() => Math.random() -  .5)
     currentQuestionIndex = 0
     questionContainerEl.classList.remove('hide')
-    // countdown();
     timerId = setInterval(clockTick, 1000)
+    time = 90
     timerEl.textContent = time;
     clockTick();
     setNextQuestion();
-}
-
-function stopGame() {
-    clearInterval(timerEl);
-
-    //
-
 }
 
 function setNextQuestion() {
@@ -101,55 +72,19 @@ function setNextQuestion() {
 
 function showQuestion(question) {
     //LOADS QUESTIONS
-    if (shuffledQuestions.length >= currentQuestionIndex + 1) {
         questionEl.innerText = question.question
         question.answers.forEach(answer => {
             var button = document.createElement('button')
             button.innerText = answer.text
             button.classList.add('btn')
             if (answer.correct) {
+                button.dataset.correct = answer.correct
                 button.classList.add('correct')            
             } else {
                 button.classList.add('false')
             }
             answerButtonsEl.appendChild(button)
-        })
-    } 
-    //IF NO QUESTIONS LEFT, PROMPT TO SAVE
-    else {
-        questionEl.innerText = "No more questions left. Congratulations!"
-        window.confirm("Would you like to save your score?")
-        if(window.confirm) {
-            saveScore();
-        } else {
-            resetGame();
-        }
-
-        //Need to kill timer HERE?
-        startButtonEl.innerText = 'Restart'
-        startButtonEl.classList.remove('hide')
-
-    }  
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element) 
-    if (correct) {
-        element.classList.add('correct')
-    } else { 
-        element.classList.add('wrong')
-    }
-}
+})}
 
 function resetState() {
     while (answerButtonsEl.firstChild) {
@@ -157,14 +92,53 @@ function resetState() {
     }
 }
 
+function endGame() {
+    questionEl.innerText = "No more questions left. Congratulations!"
+    var msgSave = "Would you like to save your score?";
+    var msgTryAgain = "Would you like to try again?";
+    if (confirm(msgSave)) {
+        saveScore()
+    } 
+    if (confirm(msgTryAgain)) {
+        restartGame();
+    }
+}
+
+function restartGame() {
+    resetState();
+    shuffledQuestions = questions.sort(() => Math.random() -  .5)
+    currentQuestionIndex = 0
+    time = 91
+    setNextQuestion();
+}
+
 function saveScore() { 
     var initials = window.prompt("Please enter your initials!")
-    var score = timeLeft
-console.log(initials, score)
-    localStorage.setItem('Initials', initials)
-    localStorage.setItem('Score', score)
+    const score = {
+        name: initials,
+        timeScore: time
+    };
+    highScoresArr.push(JSON.stringify(score));
+    console.log(highScoresArr);
+    localStorage.setItem("highScoresArr", highScoresArr)
+    // var msgTryAgain = "Would you like to try again?";
+    // if (confirm(msgTryAgain)) {
+    // restartGame();
+    // } else {
+    //     stopGame();
+    // }
 }
-    
+
+
+
+    // var save = []
+    // console.log(initials, score)
+    // save.push(JSON.stringify(score))
+    // save.push(initials)
+    // localStorage.setItem(initials, save)
+    // console.log(save)
+    // localStorage.setItem('Initials', initials)
+    // localStorage.setItem('Score', score)
 
 
 
@@ -177,3 +151,19 @@ console.log(initials, score)
 //     currentQuestionIndex++;
 //     setNextQuestion();
 // })
+// function setStatusClass(element, correct) {
+//     clearStatusClass(element) 
+//     if (correct) {
+//         element.classList.add('correct')
+//     } else { 
+//         element.classList.add('wrong')
+//     }
+// }    // startButtonEl.innerText = 'Restart'
+    // startButtonEl.classList.remove('hide')
+    
+    
+    //const correct = selectedButton.dataset.correct
+    // setStatusClass(document.body, correct)
+    // Array.from(answerButtonsEl.children).forEach(button => {
+    //     setStatusClass(button, button.dataset.correct)
+    // })
